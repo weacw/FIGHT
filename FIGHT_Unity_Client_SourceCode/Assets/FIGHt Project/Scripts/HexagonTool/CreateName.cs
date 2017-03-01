c1d5c47e882da65c718690daa52e844d9910802b
DESKTOP-ROTV5LC\a <446824106@qq.com>
2017-03-01 16:02:16 +0800

    Upload all files - asset

---
+++ FIGHT_Unity_Client_SourceCode/Assets/FIGHt Project/Scripts/HexagonTool/CreateName.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class CreateName : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        int index = name.IndexOf("-");
+        string Name = name.Insert(index, "-0");
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).name = Name.Replace("-0", "-" + (i + 1).ToString("D2"));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ FIGHT_Unity_Client_SourceCode/Assets/FIGHt Project/Scripts/HexagonTool/HexCell.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HexCell : MonoBehaviour
+{
+
+    void OnTriggerStay(Collider other)
+    {
+        Debug.Log("trigger");
+        DestroyObject(gameObject);
+    }
+
+}
