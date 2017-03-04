e4080ec1bbe79a896ad30d82463db270b164474e
DESKTOP-IMCTIH7\g4468 <446824106@qq.com>
2017-03-04 14:21:16 +0800

    add hexeditor and update model

---
+++ FIGHT_Unity_Client_SourceCode/Assets/Editor/HexGirdMenuEditor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class HexGirdMenuEditor : EditorWindow
+{
+    int width = 6;
+    int height = 6;
+    HexCell hex;
+
+    [MenuItem("Window/HexGird")]
+    static void Init()
+    {
+        // Get existing open window or if none, make a new one:
+        HexGirdMenuEditor window = (HexGirdMenuEditor)EditorWindow.GetWindow(typeof(HexGirdMenuEditor));
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Base Settings", EditorStyles.boldLabel);
+        width = EditorGUILayout.IntField("width", width);
+        height = EditorGUILayout.IntField("height", height);
+        hex = EditorGUILayout.ObjectField(new GUIContent("hex prefab"),hex, typeof(HexCell),false) as HexCell;
+        if (GUILayout.Button("Init hex"))
+        {
+            HexGrid grid = new HexGrid(width,height,hex);
+            grid.Init();
+        }
+    }
+}
--- FIGHT_Unity_Client_SourceCode/Assets/FIGHt Project/Scripts/HexagonTool/HexGrid.cs
+++ FIGHT_Unity_Client_SourceCode/Assets/FIGHt Project/Scripts/HexagonTool/HexGrid.cs
@@ -2,20 +2,25 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class HexGrid : MonoBehaviour
+public class HexGrid
 {
-    public int width = 6;
-    public int height = 6;
-    public HexCell cellPrefab;
-    //public Text cellLabelPrefab;
-    //Canvas gridCanvas;
-    HexCell[] cells;
-    //HexMesh hexMesh;
-    void Awake()
+    private int width = 6;
+    private int height = 6;
+    private HexCell cellPrefab;
+    private HexCell[] cells;
+    private GameObject g = new GameObject("hex gird");
+
+    public HexGrid(int width, int height, HexCell cell)
     {
-        //gridCanvas = GetComponentInChildren<Canvas>();
-        //hexMesh = GetComponentInChildren<HexMesh>();
+        this.width = width;
+        this.height = height;
+        this.cellPrefab = cell;
+    }
+    //初始化hexcell
+    public void Init()
+    {
         cells = new HexCell[height * width];
+      
         for (int z = 0, i = 0; z < height; z++)
         {
             for (int x = 0; x < width; x++)
@@ -24,25 +29,17 @@ public class HexGrid : MonoBehaviour
             }
         }
     }
-    //void Start()
-    //{
-    //    hexMesh.Triangulate(cells);
-    //}
 
-    void CreateCell(int x, int z, int i)
+    //创建Hexcell
+    private void CreateCell(int x, int z, int i)
     {
         Vector3 position;
         position.x = (x + z * 0.5f - z / 2) * (HexMetrics.innerRadius * 2f);
         position.y = 0f;
         position.z = z * (HexMetrics.outerRadius * 1.5f);
-
-        HexCell cell = cells[i] = Instantiate(cellPrefab);
-        cell.transform.SetParent(transform, false);
+        HexCell cell = cells[i] = Object.Instantiate(cellPrefab);
+        cell.transform.SetParent(g.transform, false);
         cell.transform.localPosition = position;
-        //Text label = Instantiate(cellLabelPrefab);
-        //label.rectTransform.SetParent(gridCanvas.transform, false);
-        //label.rectTransform.anchoredPosition = new Vector2(position.x, position.z);
-        //label.text = x.ToString() + "\n" + z.ToString();
     }
 
 
